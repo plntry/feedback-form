eab6325fe7b42db5505d5baf60e253df95bf8590
Oksana Nedilko <nedilko7b@gmail.com>
2022-10-11 19:09:50 +0300

    Added connection to the pgAdmin

--- server/feedback/src/app.module.ts
+++ server/feedback/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './feedbacks/feedback.controller';
-import { FeedbackService } from './feedbacks/feedback.service';
+import { FeedbackModule } from './feedbacks/feedback.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeConfigService } from './config/sequelizeConfig.service';
+import { databaseConfig } from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [FeedbackService],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    FeedbackModule,
+  ],
 })
 export class AppModule {}
--- server/feedback/src/feedbacks/feedback.module.ts
+++ server/feedback/src/feedbacks/feedback.module.ts
@@ -1,13 +1,12 @@
-import { Module } from "@nestjs/common";
-import { SequelizeModule } from "@nestjs/sequelize";
-import { FeedbackController } from "./feedback.controller";
-import { FeedbackService } from "./feedback.service";
-import { Feedback } from "./models/feedback.model";
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { FeedbackController } from './feedback.controller';
+import { FeedbackService } from './feedback.service';
+import { Feedback } from './models/feedback.model';
 
 @Module({
-    imports: [SequelizeModule.forFeature([Feedback])],
-    providers: [FeedbackService],
-    controllers: [FeedbackController,
+  imports: [SequelizeModule.forFeature([Feedback])],
+  providers: [FeedbackService],
+  controllers: [FeedbackController],
 })
-
 export class FeedbackModule {}
