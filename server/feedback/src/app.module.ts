986c9506dd7702255de8237c690facc0dc8cc55a
Oksana Nedilko <nedilko7b@gmail.com>
2022-10-14 20:30:47 +0300

    Deploy beginning

--- server/feedback/src/app.module.ts
+++ server/feedback/src/app.module.ts
@@ -4,6 +4,8 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeConfigService } from './config/sequelizeConfig.service';
 import { databaseConfig } from './config/configuration';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -13,6 +15,9 @@ import { databaseConfig } from './config/configuration';
     }),
     ConfigModule.forRoot({
       load: [databaseConfig],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'build'),
     }),
     FeedbackModule,
   ],
